a76d5ab9729e38fd28d1a0500c1f9ccf11ba78db
Matthias Binzer <matthias.binzer@de.bosch.com>
2022-05-28 22:59:08 +0200

    Clenaup a bit to allow to call methods from other parts, e.g. cli

--- importer.py
+++ importer.py
@@ -15,72 +15,95 @@ from registry_handling import delete_registry_entry_from_cx_items, upsert_regist
 from edc_handling import delete_assets_from_cx_items, upsert_assets_from_cx_items
 from dependencies import DB_ID_MAPPINGS, DB_POLICY_ID_MAPPINGS, REGISTRY_BASE_URL, DB_CX_ITEMS, SCHEMA_SERIAL_PART_TYPIZATION_LOOKUP_STRING, SCHEMA_TESTDATA_CONTAINER_LOOKUP_STRING, delete_db_all, get_first_match
 
+def get_testdata_from_file(filename):
+    # read the data
+    filedata = ''
+    with open(filename, 'r', encoding='iso-8859-1') as f:
+        filedata = f.read()
+    data = json.loads(filedata)
 
-parser = argparse.ArgumentParser(description="Import from test data set (JSON)")
-parser.add_argument('input_filename',
-        help='Input file. needs to be in format of TestDataContainer/1.0.0')
-parser.add_argument('--list-manufacturers', action='store_true',
-        help='List available ManufactuerID s')
-parser.add_argument('--import-for',
-        help='Import data for the given ManufactuerID')
-parser.add_argument('--disable-registry-entry', action='store_true',
-        help='Disables automatic registering at the AAS registry. (relevant env: REGISTRY_BASE_URL)')
-parser.add_argument('--delete',action='store_true', default=False,
-        help='Deletes items from EDC and Registry. Can not be combined with import, but also needs the Manufacturer ID.')
+    data = get_first_match(data, key_match=SCHEMA_TESTDATA_CONTAINER_LOOKUP_STRING, default_return=[])
+    return data
 
-
-args = parser.parse_args()
-print(args)
-
-if not args.input_filename:
-    parser.print_help()
-    sys.exit(1)
-
-if not os.path.isfile(args.input_filename):
-    print(f"Given input file does not exist: {args.input_filename}", file=sys.stderr)
-    sys.exit(1)
-
-# read the data
-manufacturers = set()
-filedata = ''
-with open(args.input_filename, 'r', encoding='iso-8859-1') as f:
-    filedata = f.read()
-data = json.loads(filedata)
-
-testdata = get_first_match(data, key_match=SCHEMA_TESTDATA_CONTAINER_LOOKUP_STRING, default_return=[])
-
-if args.list_manufacturers:
+def get_manufacturers(testdata):
+    manufacturers = set()
     for item in testdata:
         part_typizations = get_first_match(item=item, key_match=SCHEMA_SERIAL_PART_TYPIZATION_LOOKUP_STRING, default_return=[])
         for aspect in part_typizations:
             for localid in aspect['localIdentifiers']:
                 if localid.get('key', '') == 'ManufacturerID':
                     manufacturers.add(localid.get('value', ''))
-    print(manufacturers)
-    sys.exit(0)
+    return manufacturers
 
-if args.import_for:
-    manufacturer = args.import_for
-    db_items = {}
-
-    
+def iterate_bpn_aspects(testdata, bpn):
+    """
+    Iterate over all aspects / data elements of a given BPN.
+    To find out whether an item belongs to the BPN, we look into the SerialPartTypization localIdentifiers.
+    Thus, SerialPartTypization aspect must be available.
+    """
     for item in testdata:
         part_typizations = get_first_match(item=item, key_match=SCHEMA_SERIAL_PART_TYPIZATION_LOOKUP_STRING, default_return=[])  # TODO: we assume that every part has this aspect!
         cx_id = item.get('catenaXId', '')
         for aspect in part_typizations:
             for localid in aspect['localIdentifiers']:
-                if (localid.get('key', '') == 'ManufacturerID') and (localid.get('value', '') == manufacturer):
-                    with shelve.open(DB_CX_ITEMS) as db:
-                        db[cx_id] = item
+                if (localid.get('key', '') == 'ManufacturerID') and (localid.get('value', '') == bpn):
+                    yield item
 
-# either delete OR create, not both at the same time - for now                
-if args.delete:
-    delete_assets_from_cx_items()
-    delete_registry_entry_from_cx_items()
-    delete_db_all(dbname=DB_CX_ITEMS)
-    delete_db_all(dbname=DB_ID_MAPPINGS)
-    delete_db_all(dbname=DB_POLICY_ID_MAPPINGS)
-else:
-    upsert_assets_from_cx_items()
-    upsert_registry_entry_from_cx_items(bpn=args.import_for)
+
+###################### main ##################
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Import from test data set (JSON)")
+    parser.add_argument('input_filename',
+            help='Input file. needs to be in format of TestDataContainer/1.0.0')
+    parser.add_argument('--list-manufacturers', action='store_true',
+            help='List available ManufactuerID s')
+    parser.add_argument('--import-for',
+            help='Import data for the given ManufactuerID')
+    parser.add_argument('--disable-registry-entry', action='store_true',
+            help='Disables automatic registering at the AAS registry. (relevant env: REGISTRY_BASE_URL)')
+    parser.add_argument('--delete',action='store_true', default=False,
+            help='Deletes items from EDC and Registry. Can not be combined with import, but also needs the Manufacturer ID.')
+
+
+    args = parser.parse_args()
+    print(args)
+
+    if not args.input_filename:
+        parser.print_help()
+        sys.exit(1)
+
+    if not os.path.isfile(args.input_filename):
+        print(f"Given input file does not exist: {args.input_filename}", file=sys.stderr)
+        sys.exit(1)
+
+
+
+    testdata = get_testdata_from_file(filename=args.input_filename)
+
+    if args.list_manufacturers:
+        manufacturers = get_manufacturers(testdata)
+        print(manufacturers)
+        sys.exit(0)
+
+
+    if args.import_for:
+        manufacturer = args.import_for
+        db_items = {}
+
+        for item in iterate_bpn_aspects(testdata=testdata, bpn=manufacturer):
+            with shelve.open(DB_CX_ITEMS) as db:
+                cx_id = item.get('catenaXId', '')
+                db[cx_id] = item
+
+    # either delete OR create, not both at the same time - for now     
+    if args.delete:
+        delete_assets_from_cx_items()
+        delete_registry_entry_from_cx_items()
+        delete_db_all(dbname=DB_CX_ITEMS)
+        delete_db_all(dbname=DB_ID_MAPPINGS)
+        delete_db_all(dbname=DB_POLICY_ID_MAPPINGS)
+    else:
+        upsert_assets_from_cx_items()
+        upsert_registry_entry_from_cx_items(bpn=args.import_for)
+
 
